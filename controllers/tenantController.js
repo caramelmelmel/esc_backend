b3556c8a09ba57c37d3051bd4d6f516111dccc8a
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-04-10 21:49:15 +0800

    RIP IT WORKS

--- controllers/tenantController.js
+++ controllers/tenantController.js
@@ -1,17 +1,21 @@
 const moment = require('moment');
 
 //import dbQuery from '../db/dev/dbQuery';
-const query = require('../database/dev/dbQuery')
+const dbQuery = require('../database/dev/dbQuery')
 
 const validate = require('../helpers/validations');
 const stats = require('../helpers/status')
 const poolconfig = require('../config/database').pool;
 const jwt = require('jsonwebtoken')
+const passport = require('passport')
+const JWTstrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+const localStrategy = require('passport-local').Strategy;
 
-require('dotenv').config
+require('dotenv').config();
 
 //use the pg pool library 
-const Pool = require('pg').Pool;
+const {Pool,Client} = require('pg');
 const pool = new Pool({
   user: process.env.PGUSER,
 	host: process.env.PGHOST,
@@ -22,18 +26,6 @@ const pool = new Pool({
 })
 
 //create query function here
-
-
-const generateUserToken = (tenant_name,tenant_email,institution_id,password) => {
-  const token = jwt.sign({
-    tenant_name,
-    tenant_email,
-    institution_id,
-    password
-  },
-  process.env.TENANT_TOKEN_SECRET, { expiresIn: '1800s' });
-  return token;
-};
 
 const createTenant = async (req,res)=>{
 
@@ -62,46 +54,48 @@ const createTenant = async (req,res)=>{
         return res.status(stats.status.bad).send(stats.errorMessage);
       }
       const hashedPassword = validate.hashPassword(req.body.password);
-      //console.log('After pw hashing')
-      //INSERT query into other tables 
-      //const insStore = `INSERT INTO stores (store_name) values ($1)`
-      //select query from other tables
-      const selcat = `SELECT category FROM category where category_name = $1 `
-      const selInst = `SELECT institution_id FROM singhealth_institutions where institution_name = $1 `
-      const selstore = `SELECT store_name FROM stores where store_name =$1 RETURNING store_name`
-
       //query to insert tenant on success
       const createTenantQuery = `INSERT INTO tenant(tenant_name,category_ID, store_des,email, expiry_date,password,store_name,institution_id)
                                 values($1, $2, $3, $4, $5, $6, $7, $8) returning *`
-
-      //const storename = [req.body.store_name]
       
+      // need to return category ID
+
+    const result = await pool.query('select institution_id from singhealth_institutions where institution_name = $1', [req.body.institution_name]);
+
+    const instid= result.rows[0].institution_id
+
+    //console.log(`${req.body.category_name}`)
+    const result2 = await pool.query('select category_id from category where category_name = $1', [req.body.category_name]);
+
+    const categoryID = result2.rows[0].category_id
+
+    console.log(`${categoryID}`)
+
+      //need to return the s
       const values = [
         req.body.tenant_name,
-        req.body.category_name, 
+        categoryID, 
         req.body.store_des,
         req.body.email,
         req.body.expiry_date,
         hashedPassword,
         req.body.store_name,
-        req.body.institution_name 
-      ] 
-      values[1] = pool.query(selcat,[req.body.category_name])[0];
-      console.log(`${values[1]}`)
-      console.log('query inserted successfully')
-      values[7] = pool.query(selInst,[req.body.institution_name])[0];  
-      console.log('before try')
+        instid
+      ]
+      console.log('query selected successfully')
+      console.log('before try') 
 
       try {
         console.log('inserting values')
-        const { rows } = await pool.query(createTenantQuery, values);
+        const { rows } = await dbQuery.query(createTenantQuery, values)
+        console.log(typeof rows)
         console.log('There is db response')
 
         const dbResponse = rows[0];
         delete dbResponse.password;
-        const token = generateUserToken(dbResponse.tenant_name,dbResponse.tenant_email,dbResponse.institution_id,dbResponse.password);
+        //const token = generateUserToken(dbResponse.tenant_name,dbResponse.tenant_email,dbResponse.institution_id,dbResponse.password);
         stats.successMessage.data = dbResponse;
-        stats.successMessage.data.token = token;
+        //stats.successMessage.data.token = token;
 
         return res.status(stats.status.created).send(stats.successMessage);
 
@@ -125,42 +119,47 @@ const createTenant = async (req,res)=>{
    * @returns {object} user object
    */
 const signinTenant = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
     
-    if (validate.isEmpty(email) || validate.isEmpty(password)) {
-      stats.errorMessage.error = 'Email or Password detail is missing';
-      return res.status(stats.status.bad).send(stats.errorMessage);
+  if (validate.isEmpty(email) || validate.isEmpty(password)) {
+    stats.errorMessage.error = 'Email or Password detail is missing';
+    return res.status(stats.status.bad).send(stats.errorMessage);
+  }
+  if (!validate.isValidEmail(email) || !validate.validatePassword(password)) {
+    stats.errorMessage.error = 'Please enter a valid Email or Password';
+    return res.status(stats.status.bad).send(stats.errorMessage);
+  }
+
+  const signinTenantQuery = 'SELECT * FROM tenant WHERE email = $1';
+  try {
+    const { rows } = await pool.query(signinTenantQuery, [req.body.email]);
+    const dbResponse = rows[0];
+
+    if (!dbResponse) {
+      stats.errorMessage.error = 'User with this email does not exist';
+      return res.status(stats.status.notfound).send(stats.errorMessage);
     }
-    if (!validate.isValidEmail(email) || !validate.validatePassword(password)) {
-      stats.errorMessage.error = 'Please enter a valid Email or Password';
-      return res.status(stats.status.bad).send(stats.errorMessage);
+
+    if (!validate.comparePassword(dbResponse.password, password)) {
+      stats.errorMessage.error = 'The password you provided is incorrect';
+      return res.status(status.bad).send(stats.errorMessage);
     }
+    const token =  jwt.sign({ id: dbResponse.tenant_id }, process.env.TENANT_TOKEN_SECRET, {
+      expiresIn: 86400 // 24 hours
+    });
+    delete dbResponse.password;
+    stats.successMessage.data = dbResponse;
+    stats.successMessage.data.token = token;
+    return res.header('x-auth-token',token).send(stats.successMessage.data.token);
+  } catch (error) {
+    errorMessage.error = 'Operation was not successful';
+    return res.status(stats.status.error).send(stats.errorMessage);
+  }
 
-    const signinTenantQuery = 'SELECT * FROM tenant WHERE email = $1';
-    try {
-      const { rows } = await pool.query(signinTenantQuery, [req.body.email]);
-      const dbResponse = rows[0];
 
-      if (!dbResponse) {
-        stats.errorMessage.error = 'User with this email does not exist';
-        return res.status(stats.status.notfound).send(stats.errorMessage);
-      }
+  }
 
-      if (!validate.comparePassword(dbResponse.password, password)) {
-        stats.errorMessage.error = 'The password you provided is incorrect';
-        return res.status(status.bad).send(stats.errorMessage);
-      }
-      const token = generateUserToken(dbResponse.name,dbResponse.email,dbResponse.institution_id,dbResponse.password);
-      delete dbResponse.password;
-      stats.successMessage.data = dbResponse;
-      stats.successMessage.data.token = token;
-      return res.status(stats.status.success).send(stats.successMessage);
-    } catch (error) {
-      errorMessage.error = 'Operation was not successful';
-      return res.status(stats.status.error).send(stats.errorMessage);
-    }
-  };
-
+  
   module.exports = {
       createTenant,
       signinTenant
